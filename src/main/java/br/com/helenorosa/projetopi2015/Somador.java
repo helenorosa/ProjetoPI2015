be21f584afaf571b1332e1e66646443e5520f905
unknown <helenorosa>
2015-07-01 12:13:28 -0300

    criando testes novos testes

--- src/main/java/br/com/helenorosa/projetopi2015/Somador.java
+++ src/main/java/br/com/helenorosa/projetopi2015/Somador.java
@@ -7,14 +7,4 @@ public class Somador {
         return numero1 + numero2;
     }
     
-    public Integer somarNumeros( Integer numero ) {
-        Integer n = numero + 2;
-        Integer n1 = numero + 3;
-        Integer n2 = numero + 4;
-        Integer n3 = numero + 5;
-        Integer n4 = numero + 6;
-        Integer n5 = numero + 7;
-        return null;
-        
-    }
 }
